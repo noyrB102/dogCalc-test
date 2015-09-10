ab641955da67631de9da26af04e9d4eb9d8289d3
Bryon Swanson <bryonswanson@bryons-mac-mini.local>
2015-09-10 07:58:33 -0600

    Test code for GitHub

--- AProject/ViewController.swift
+++ AProject/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var age: UITextField!
+    @IBAction func calculateAge(sender: AnyObject) {
+        
+        let myAge = Int(age.text!)! * 7
+        resultsLabel.text = "I'm \(myAge) years old"
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
